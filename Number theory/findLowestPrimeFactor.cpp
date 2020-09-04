d1de859e4cead6eaa62644834dd3fc9e1c1c471b
shahazad <shanudjn@gmail.com>
2020-09-04 22:15:23 +0530

    find lowest prime factor from 1 to n

---
+++ Number theory/findLowestPrimeFactor.cpp
@@ -0,0 +1,32 @@
+//find lowest prime factor for every number from 1 to n
+
+#include<iostream>
+using namespace std;
+
+void lowestPrimefactor(int n, int * p){
+  p[0] = 1;
+  p[1] = 1;
+  for(int i = 2 ; i <= n; i++){
+    if(p[i] == 0){
+      for(int j = 2; i*j <= n; j++){
+        if(p[i*j] == 0){
+          p[i*j] = i;
+        }
+      }
+    }
+  }
+}
+
+int main(){
+  int n;
+  cin >> n;
+  int * p = new int[n+1];
+  for(int i = 0; i <= n; i++){
+    p[i] = 0;
+  }
+  lowestPrimefactor(n,p);
+  for(int i = 1; i <= n; i++){
+    cout << i << " " << p[i] << endl;
+  }
+
+}
